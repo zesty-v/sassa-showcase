aefe63bc8460fa1563fe88fa84be67cb3c4b4d6c
zesty-v <sytze@futurism.co.za>
2023-11-05 07:43:30 +0200

    Moveing from HTML design to php

--- js/sassafunctions.js
+++ js/sassafunctions.js
@@ -15,3 +15,58 @@ function toggleImageSrc(imgElement, altSrc) {
         imgElement.setAttribute('src', altSrc);
     }
 }
+
+function isValidSAID(number) {
+  // Check for the correct length
+  if (number.length !== 13) return false;
+
+  // Extract the date of birth from the ID
+  let dob = number.substring(0, 6);
+  let year = parseInt(dob.substring(0, 2), 10);
+  let month = parseInt(dob.substring(2, 4), 10) - 1; // Month is 0-indexed in JS
+  let day = parseInt(dob.substring(4, 6), 10);
+
+  // Check the validity of the date
+  let dateOfBirth = new Date(year + 1900, month, day);
+  if (dateOfBirth.getFullYear() != year + 1900 ||
+      dateOfBirth.getMonth() != month ||
+      dateOfBirth.getDate() != day) {
+    return false;
+  }
+
+  // Luhn algorithm check (modulus 10)
+  let total = 0;
+  let count = 0;
+
+  for (let i = 0; i < 13; i++) {
+    let digit = parseInt(number.charAt(i), 10);
+    if (isNaN(digit)) return false;
+    if (count % 2 !== 0) {
+      digit *= 2;
+      if (digit > 9) digit -= 9;
+    }
+    total += digit;
+    count++;
+  }
+
+  return (total % 10 === 0);
+}
+
+function formatInput(input) {
+  let numbers = input.value.replace(/\D/g, '');
+
+  // If the input is not a valid SA ID number, do nothing
+  if (!isValidSAID(numbers)) {
+    // Optionally, provide user feedback that the ID is invalid
+    return;
+  }
+
+  let sections = [];
+  sections.push(numbers.substring(0, 6)); // First 6 digits
+  if (numbers.length > 6) sections.push(numbers.substring(6, 10)); // Next 4 digits
+  if (numbers.length > 10) sections.push(numbers.substring(10, 12)); // Next 2 digits
+  if (numbers.length > 12) sections.push(numbers.substring(12, 13)); // Next digit
+
+  input.value = sections.join(' ').trim();
+	
+}
---
+++ menu.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SASSA - New Grant Options</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+
+	<!-- Custom CSS -->
+	<link href="css/sassa-custom.css" rel="stylsheet">
+
+	</head>
+
+	<body>
+	<div class="jumbotron jumbotron-fluid text-center">
+	   <img src="images/sassa-logo1.png" alt="" class="img-fluid">
+		<div class="container">
+	<hr class="my-4">
+			<label for="submitbtn"><br> <b>SA ID Number:</b>&nbsp;&nbsp;&nbsp;&nbsp;</label>
+				<input type="text" id="numberInput" oninput="formatInput(this)" placeholder="711005 5084 08 1" pattern="\d*">
+				<input type="submit" id="submitbtn" value="Go">
+	<hr class="my-4">
+			<div class="row">
+				<div class="col-sm"><img src="images/1.0.caredependency.png" onmouseover="toggleImageSrc(this, 'images/1.1.caredependency.png');" alt="Care Dependency" onmouseout="toggleImageSrc(this, 'images/1.0.caredependency.png');" class="toggleImage img-fluid max-size-210"><br>Care Dependency<br><br></div>
+				<div class="col-sm"><img src="images/2.0.childcare.png" onmouseover="toggleImageSrc(this, 'images/2.1.childcare.png');" alt="Child Support" onmouseout="toggleImageSrc(this, 'images/2.0.childcare.png');" class="toggleImage img-fluid max-size-210"><br>Child Support<br><br></div>
+				<div class="col-sm"><img src="images/3.0.fosterchild.png" onmouseover="toggleImageSrc(this, 'images/3.1.fosterchild.png');" alt="Foster Child" onmouseout="toggleImageSrc(this, 'images/3.0.fosterchild.png');" class="toggleImage img-fluid max-size-210"><br>Foster Child<br><br></div>
+			</div>
+			<div class="row">
+				<div class="col-sm"><img src="images/4.0.disability.png" onmouseover="toggleImageSrc(this, 'images/4.1.disability.png');" alt="Disability" onmouseout="toggleImageSrc(this, 'images/4.0.disability.png');" class="toggleImage img-fluid max-size-210"><br>Disability<br><br></div>
+				<div class="col-sm"><img src="images/5.0.grantinaid.png" onmouseover="toggleImageSrc(this, 'images/5.1.grantinaid.png');" alt="Grant-in-Aid" onmouseout="toggleImageSrc(this, 'images/5.0.grantinaid.png');" class="toggleImage img-fluid max-size-210"><br>Grant-in-Aid<br><br></div>
+				<div class="col-sm"><img src="images/6.0.oldagegrant.png" onmouseover="toggleImageSrc(this, 'images/6.1.oldagegrant.png');" alt="Older Persons" onmouseout="toggleImageSrc(this, 'images/6.0.oldagegrant.png');" class="toggleImage img-fluid max-size-210"><br>Older Persons<br><br></div>
+			</div>
+			<div class="row">
+				<div class="col-sm"><img src="images/7.0.reliefofdistress.png" onmouseover="toggleImageSrc(this, 'images/7.1.reliefofdistress.png');" alt="Relief of Distress" onmouseout="toggleImageSrc(this, 'images/7.0.reliefofdistress.png');" class="toggleImage img-fluid max-size-210"><br>Relief of Distress<br></div>
+				<div class="col-sm"><img src="images/8.0.warveterans.png" onmouseover="toggleImageSrc(this, 'images/8.1.warveterans.png');" alt="War Veterans" onmouseout="toggleImageSrc(this, 'images/8.0.warveterans.png');" class="toggleImage img-fluid max-size-210"><br>War Veterans<br></div>
+				<div class="col-sm"><img src="images/9.0.caredependencycovid.png" onmouseover="toggleImageSrc(this, 'images/9.1.caredependencycovid.png');" alt="Relief of Distress" onmouseout="toggleImageSrc(this, 'images/9.0.caredependencycovid.png');" class="toggleImage img-fluid max-size-210"><br>Covid-19 Relief of Distress<br><br></div>
+			</div>
+		</div>
+    </div>
+    <div class="container">
+       <div class="row">
+		  <p class="text-center"><span class="badge badge-info"></span></p>
+          <div class="text-center col-lg-6 offset-lg-3">
+            <p>Copyright &copy; 2023 &middot; All Rights Reserved.
+				<br><span class="badge badge-danger">logged out</span></p>
+          </div>
+
+       </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+    <script src="js/jquery-3.4.1.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed --> 
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap-4.4.1.js"></script>
+	  
+	<!-- Custom functions for SASSA -->
+    <script src="js/sassafunctions.js"></script>
+	  
+    </body>
+</html>
